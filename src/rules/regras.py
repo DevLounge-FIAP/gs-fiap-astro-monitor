414d1deec9acbaa0aeccaf604db32b6ba14b2f8d
Aelton0 <aeltonsoaresdemenezes@gmail.com>
2026-05-29 20:29:33 -0300

    Remove MotorDeRegras class definition from regras.py to streamline alert system integration

--- src/rules/regras.py
+++ src/rules/regras.py
@@ -6,8 +6,3 @@ class CentraldDeAlertas(modulos.Missao_espacial):
         self.prioridade = prioridade
         self.mensagem = mensagem
 
-class MotorDeRegras(Sistema, CentralDeAlertas):
-
-    def __init__(self, missao, central_alerta):
-        self.missao= missao
-        self.central_alertas= central_alerta
